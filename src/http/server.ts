362ef91d9771d1566475aad4d58c86da705e2c56
Thigo <thi.it.solutions@gmail.com>
2024-09-08 13:40:04 -0300

    creating server

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,11 @@
-console.log('hello world!')
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/', () => {
+  return { message: "Hello, Fastify!" };
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server is running on port 3333");
+})
