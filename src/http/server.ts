b226917f9961e75508b7e355e0d8f9962d089118
Thigo <thi.it.solutions@gmail.com>
2024-09-08 15:59:29 -0300

    chore(): testing router

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,28 @@
-import fastify from "fastify";
+import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
 
 const app = fastify()
 
-app.get('/', () => {
-  return { message: "Hello, Fastify!" };
+const prisma = new PrismaClient
+
+app.post('/polls', async (request, reply) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  })
+
+  const { title } = createPollBody.parse(request.body)
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  })
+
+  return reply.status(201).send({ pollId: poll.id })
+
 })
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("Server is running on port 3333");
+  console.log('Server is running on port 3333');
 })
