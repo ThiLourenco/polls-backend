9bce7b6e1d89089b22305fb346d7d25b6d8b4a48
Thigo <thi.it.solutions@gmail.com>
2024-09-08 16:37:31 -0300

    chore(): adding vote-on-poll router

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,13 @@
 import fastify from 'fastify';
 import { createPoll } from './routes/create-poll'
 import { getPollById } from './routes/get-poll';
+import { voteOnPoll } from './routes/vote-on-poll';
 
 const app = fastify()
 
 app.register(createPoll)
 app.register(getPollById)
+app.register(voteOnPoll)
 
  app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333');
