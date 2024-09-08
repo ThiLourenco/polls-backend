307864b39f519d97551018b8105b8e1d1f77d583
Thigo <thi.it.solutions@gmail.com>
2024-09-08 16:31:54 -0300

    chore(): adding get-poll router

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify';
 import { createPoll } from './routes/create-poll'
+import { getPollById } from './routes/get-poll';
 
 const app = fastify()
 
 app.register(createPoll)
+app.register(getPollById)
 
  app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333');
