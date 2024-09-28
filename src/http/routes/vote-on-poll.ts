c8a0d2a5a1588091a9c2c13dab575daea9852ecd
Thigo <thi.it.solutions@gmail.com>
2024-09-28 12:45:46 -0300

    chore(): adding return title

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -45,7 +45,7 @@ export async function voteOnPoll(app: FastifyInstance) {
         })
 
       } else if (userPreviousVoteOnPoll) {
-        return reply.status(400).send({message: 'Você já votou nesta canditado'})
+        return reply.status(400).send({message: 'Você já votou nesse canditado'})
       }
     }
 
@@ -71,11 +71,23 @@ export async function voteOnPoll(app: FastifyInstance) {
     // increment one in each poll
     const votes = await redis.zincrby(pollId, 1, pollOptionId) 
 
+    // return title 
+    const option = await prisma.pollOption.findUnique({
+      where: {
+        id: pollOptionId,
+      },
+      select: {
+        title: true,
+      },
+    });
+
     voting.publish(pollId, {
       pollOptionId,
       votes: Number(votes),
     })
     
-    return reply.status(201).send()
+    return reply.status(201).send({
+      optionTitle: option?.title
+    })
   })
 }
